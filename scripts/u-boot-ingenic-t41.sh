d4844fa702eac724f4b4172e4e59b7618ca4a5a7
cronyx <xcronyx@gmail.com>
2023-11-07 10:05:36 +0300

    only nor

--- scripts/u-boot-ingenic-t41.sh
+++ scripts/u-boot-ingenic-t41.sh
@@ -4,19 +4,12 @@ export CROSS_COMPILE=mipsel-linux-gnu-
 
 declare -A cfg
 cfg[t41l]="isvp_t41l_sfc_nor"
-cfg[t41l-nand]="isvp_t41l_sfc0_nand"
 cfg[t41lq]="isvp_t41lq_sfc_nor"
-cfg[t41lq-nand]="isvp_t41lq_sfc0_nand"
 cfg[t41n]="isvp_t41n_sfc_nor"
-cfg[t41n-nand]="isvp_t41n_sfc0_nand"
 cfg[t41nq]="isvp_t41nq_sfc_nor"
-cfg[t41nq-nand]="isvp_t41nq_sfc0_nand"
 cfg[t41a]="isvp_t41a_sfc_nor"
-cfg[t41a-nand]="isvp_t41a_sfc0_nand"
 cfg[t41zx]="isvp_t41zx_sfc_nor"
-cfg[t41zx-nand]="isvp_t41zx_sfc0_nand"
 cfg[t41xq]="isvp_t41xq_sfc_nor"
-cfg[t41xq-nand]="isvp_t41xq_sfc0_nand"
 
 ln -s compiler-gcc7.h include/linux/compiler-gcc10.h
 mkdir -p ../ingenic-output
